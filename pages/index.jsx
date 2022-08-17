07b69e57370bcac3d00cd92c9323bae42b0aaa90
neal8812 <111421917+neal8812@users.noreply.github.com>
2022-08-17 16:44:00 +0800

    1

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "uoXdAvIwvjNNgSXC0HF26nH15amouLaa"
+    const api_key = "0x9899a2aA32AF65458b69556c6834C66B83fa1217"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
